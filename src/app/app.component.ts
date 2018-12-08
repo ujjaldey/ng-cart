da71274a9418e36ef22e8c8bcafcb47029a0bae6
Ujjal Dey <ujjaldey@gmail.com>
2018-12-08 14:08:42 +0800

    End of "Introduction To Angular"

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ng-cart';
+  title = '';
+  url = '';
+  listItems: any;
+
+  constructor() {
+    this.listItems = [{
+      name: "Home",
+      link: '#/home'
+    }, {
+      name: "Products",
+      link: '#/products'
+    }, {
+      name: "Cart",
+      link: '#/cart'
+    }, {
+      name: "About",
+      link: '#/about'
+    }];
+  }
+
+  add(title, url) {
+    if (title != '' && url != '') {
+      url = 'http://' + url;
+
+      this.listItems.push({
+        name: title,
+        link: url,
+        isNew: true
+      });
+
+      this.title = '';
+      this.url = '';
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { AboutComponent } from './about/about.component';
+// import { ProductsComponent } from './products/products.component';
+// import { HomeComponent } from './home/home.component';
+// import { CartComponent } from './cart/cart.component';
+// import { SidebarComponent } from './sidebar/sidebar.component';
+import { routes } from './app.router';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpModule,
+    routes
   ],
-  providers: [],
+  providers: [
+    {
+      provide: LocationStrategy,
+      useClass: HashLocationStrategy
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
